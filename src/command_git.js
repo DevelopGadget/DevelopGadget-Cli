1a528f1e5f36ab0fc501f6b958b18c69321de823
Fernando Araujo <developgadget@gmail.com>
2020-10-22 21:35:39 -0500

    test for branch

--- src/command_git.js
+++ src/command_git.js
@@ -10,10 +10,12 @@ function showError(error) {
 module.exports = {
     gitPush: async (platforms = '') => {
         if (shell.which('git')) {
+            
             if (shell.exec('git add -A').code !== 0) {
                 showError('git Add -A');
                 return;
             }
+
             const data = await inquirer.prompt([{ name: 'commit', type: 'input', message: 'Input your message commit: ' }]);
             if (shell.exec(`git commit -m "${platforms} ${data.commit}"`).code !== 0) {
                 showError(`git commit -m "${platforms} ${data.commit}"`);
@@ -31,7 +33,8 @@ module.exports = {
 
             console.log(`\n`);
 
-            console.log(`Your Push Changes In Brnach: ${red.blue.bold(stdout)}`);
+            console.log(`------ Your Changes Is Remote Branch ${blue.bold(stdout)}------\n`);
+
         }
     }
 }
